4ec1e24f4e58c96c8efb1d83466565a428d3b65f
gabriel-waltmann <gabrielwaltmann@gmail.com>
2024-12-14 11:51:45 -0300

    feat: add http handle

--- APIHandler.java
+++ APIHandler.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -7,15 +9,29 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 
 public class APIHandler {
-    public HttpResponse<String> GET(String url, int timeout) {
+    class Response<T> {
+        T data;
+
+        public Response(T data) {
+            this.data = data;
+        }
+    }
+
+    public <T> Response<T> GET(String url) {
         try {
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
-                .timeout(Duration.ofSeconds(timeout))
+                .timeout(Duration.ofSeconds(5))
                 .build();
 
-            return client.send(request, HttpResponse.BodyHandlers.ofString());
+            String response = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            T data = objectMapper.readValue(response, (Class<T>) User[].class);
+
+            return new Response<>(data);
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
--- Main.java
+++ Main.java
@@ -4,13 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
         APIHandler apiHandler = new APIHandler();
-        String response = apiHandler.GET("https://jsonplaceholder.typicode.com/users", 5).body();
 
         try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            User[] users = objectMapper.readValue(response, User[].class);
+            APIHandler.Response<User[]> response = apiHandler
+                .GET("https://jsonplaceholder.typicode.com/users");
 
-            System.out.println(users[0].name);
+            System.out.println(response.data[0].name);
         } catch (Exception e) {
             System.out.println(e);
         }
