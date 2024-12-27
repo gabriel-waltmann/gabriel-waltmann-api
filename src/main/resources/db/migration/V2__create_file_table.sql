33ade3bcb792a9ffcc1541a5598f9baa2392fd12
gabriel-waltmann <gabrielwaltmann@gmail.com>
2024-12-27 18:44:59 -0300

    ci: fix edited migration

--- src/main/resources/db/migration/V2__create_file_table.sql
+++ src/main/resources/db/migration/V2__create_file_table.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE TABLE files (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    `key` VARCHAR(100) NOT NULL,
+    key VARCHAR(100) NOT NULL,
     updated_at TIMESTAMP,
     created_at TIMESTAMP NOT NULL
 );
