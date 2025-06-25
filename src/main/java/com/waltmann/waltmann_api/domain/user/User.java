2b65a3f94cee711bad48cc9cba5a1e4cabe8f2b6
gabriel-waltmann <gabrielwaltmann@gmail.com>
2025-06-25 07:44:09 -0300

    wip

---
+++ src/main/java/com/waltmann/waltmann_api/domain/user/User.java
@@ -0,0 +1,40 @@
+package com.waltmann.waltmann_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "updated_at") @UpdateTimestamp
+    private Date updated_at;
+
+    @Column(name = "created_at") @CreationTimestamp
+    private Date created_at;
+
+}
---
+++ src/main/resources/db/migration/V10_create_users_table.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE users (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(500) NOT NULL,
+    updated_at TIMESTAMP,
+    created_at TIMESTAMP NOT NULL
+);
