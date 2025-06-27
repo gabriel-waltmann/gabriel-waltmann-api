54238f134b38ef2df2c62d4e373dd4f0497a4b84
gabriel-waltmann <gabrielwaltmann@gmail.com>
2025-06-27 20:15:25 -0300

    feat: add root route

---
+++ src/main/java/com/waltmann/waltmann_api/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.waltmann.waltmann_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+    @GetMapping
+    public String index() {
+        return "Welcome to Waltmann API!";
+    }
+}
